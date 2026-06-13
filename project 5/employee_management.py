11cef971a1455748f7e82ff412e6b211242deb38
Gangani Krishna <ganganikrishna42@gmail.com>
2026-06-13 18:42:13 +0530

    second

--- project 5/employee_management.py
+++ project 5/employee_management.py
@@ -1,5 +1,7 @@
-
 emp_dic = {}
+manager = {}
+develop = {}
+
 
 class Employee:
 
@@ -8,7 +10,7 @@ class Employee:
         self.age = age
         self.__salary = salary
         self.__employee_id = employee_id
-    
+
     def get_salary(self):
         return self.__salary
 
@@ -37,54 +39,99 @@ class Manager(Employee):
 
     def display(self):
         super().display()
-        print("Department  :", self.department)
+        print("Department :", self.department)
+
+
+class Developer(Manager):
+
+    def __init__(self, name, age, salary, employee_id, department, language):
+        super().__init__(name, age, salary, employee_id, department)
+        self.language = language
+
+    def display(self):
+        super().display()
+        print("Language :", self.language)
 
 
 while True:
 
-    
-    print(" Employee Management System:\n")
+    print("\nEmployee Management System:\n")
     print("1. Create Employee")
     print("2. Create Manager")
-    print("3. Show All Employees")
-    print("4. Check Inheritence")
-    print("5. Exit")
+    print("3. Create Developer")
+    print("4. Show All Employees")
+    print("5. Check Inheritance")
+    print("6. Exit")
 
-    choice = int(input("Enter your choice:"))
+    choice = int(input("Enter your choice: "))
+
     match choice:
+
         case 1:
 
-            name = input("Enter your Name: ")
-            age = int(input("Enter the Age: "))
-            salary = int(input("Enter the Salary: "))
-            emp_id = input("Enter your Employee ID: ")
+            name = input("Enter Name: ")
+            age = int(input("Enter Age: "))
+            salary = int(input("Enter Salary: "))
+            emp_id = input("Enter Employee ID: ")
 
             emp = Employee(name, age, salary, emp_id)
             emp_dic[name] = emp
 
-            print("\nEmploy Created Successfully!")
+            print("\nEmployee Created Successfully!")
+
         case 2:
+
             name = input("Enter Name: ")
             age = int(input("Enter Age: "))
             salary = int(input("Enter Salary: "))
-            emp_id = input("Enter Employ ID: ")
+            emp_id = input("Enter Employee ID: ")
             dept = input("Enter Department: ")
 
             mgr = Manager(name, age, salary, emp_id, dept)
-            emp_dic[name] = mgr
+            manager[name] = mgr
 
             print("\nManager Created Successfully!")
+
         case 3:
-            if not emp_dic:
-                print("\nNot  Records Found.")
+
+            name = input("Enter Name: ")
+            age = int(input("Enter Age: "))
+            salary = int(input("Enter Salary: "))
+            emp_id = input("Enter Employee ID: ")
+            dept = input("Enter Department: ")
+            lang = input("Enter Programming Language: ")
+
+            dev = Developer(name, age, salary, emp_id, dept, lang)
+            develop[name] = dev
+
+            print("\nDeveloper Created Successfully!")
+
+        case 4:
+
+            if not emp_dic and not manager and not develop:
+                print("\nNo Records Found.")
             else:
                 for obj in emp_dic.values():
                     obj.display()
-        case 4:
-            print("\nInheritance Check")
-            print("Manager is subclass of Employ :", issubclass(Manager, Employee))       
+
+                for obj in manager.values():
+                    obj.display()
+
+                for obj in develop.values():
+                    obj.display()
+
         case 5:
+
+            print("\nInheritance Check")
+            print("Manager is subclass of Employee :", issubclass(Manager, Employee))
+            print("Developer is subclass of Employee :", issubclass(Developer, Employee))
+            print("Developer is subclass of Manager :", issubclass(Developer, Manager))
+
+        case 6:
+
             print("\nThank You for Using Employee Management System.")
             break
 
-        
+        case _:
+
+            print("\nInvalid Choice.")
